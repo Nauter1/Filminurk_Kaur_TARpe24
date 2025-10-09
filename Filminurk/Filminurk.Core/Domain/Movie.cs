908f15327e13f0cae16acf71a6c0c16eee790b3a
opilane <opilane@HP-CZC3217061>
2025-10-09 10:36:39 +0300

    Created and fixed bugs

---
+++ Filminurk/Filminurk.Core/Domain/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Domain
+{
+    public class Movie
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateOnly FirstPublished { get; set; }
+        public string Director { get; set; }
+        public List<string>? Actors { get; set; }
+        public decimal? CurrentRating { get; set; }
+
+        public string? Genre { get; set; }
+        public string? Tagline { get; set; }
+        public string? Warnings { get; set; }
+    }
+}
