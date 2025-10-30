f4d8f78b06b2a21c914ed8192268b63a44dac8f1
opilane <opilane@HP-CZC3217061>
2025-10-30 08:55:21 +0200

    Created ActorGender and filled out Actors info

--- Filminurk/Filminurk.Core/Domain/Actors.cs
+++ Filminurk/Filminurk.Core/Domain/Actors.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Filminurk.Core.Domain
 {
-    internal class Actors
+    public class Actors
     {
+        public Guid ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public List<string> MoviesActedFor { get; set; }
+        public string PortraitID { get; set; }
+        
+        /* Kolm minu mõeldud asju */
+        public int? ActorRating { get; set; }
+        public Gender? Gender { get; set; }
+        public Genre? FavoriteGenre { get; set; }
+
     }
 }
