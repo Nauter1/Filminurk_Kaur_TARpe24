d55591c563fd49e35d128a2ce065aac1fed28a35
opilane <opilane@HP-CZC3217061>
2025-12-15 14:05:06 +0200

    Possibly finished 9.4, will have to test next time

--- Filminurk/Filminurk/Controllers/SignalRChatController.cs
+++ Filminurk/Filminurk/Controllers/SignalRChatController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Filminurk.Core.Domain;
+using Filminurk.Core.ServiceInterface;
+using Filminurk.Data;
+using Filminurk.Models.Accounts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Filminurk.Controllers
 {
     public class SignalRChatController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public SignalRChatController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
-            return View();
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var vm = new ChatViewModel { };
+
+            vm.DisplayName = user.AvatarName;
+           
+            return View("Index", vm);
         }
     }
 }
---
+++ Filminurk/Filminurk/Models/Accounts/ChatViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Filminurk.Models.Accounts
+{
+    public class ChatViewModel
+    {
+        public string DisplayName { get; set; }
+        //public bool ProfileType { get; set; }
+    }
+}
