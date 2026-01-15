5eaffb74fa10f205f6d20d020b411db627e4941c
opilane <opilane@HP-CZC3217061>
2026-01-15 14:21:43 +0200

    Test commit, parandasin Environment.cs

--- Filminurk/Environment.cs
+++ Filminurk/Environment.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-
+            /*
 public class Environment
 {
 	public Environment()
     { }
-    public string smtpaddress = "smtp.gmail.com";
-    public string gmailusername = "Kaur.Pakaste";
-    public string gmailpassword = "nneo ywpu psme tfww";
+    public string smtpaddress = "";
+    public string gmailusername = "";
+    public string gmailpassword = "";
 }
+             */
